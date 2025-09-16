4b5cb55be0cb0608937b482b6a756c059fae0ca0
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2025-09-16 18:02:17 +0700

    timeline service

--- services/timeline-services.ts
+++ services/timeline-services.ts
@@ -1,8 +1,12 @@
 import { httpClient } from "@/lib/http";
-import { CreateTimelineBody } from "@/schemas/timeline";
+import { CreateTimelineBody, GetTimeLines } from "@/schemas/timeline";
 
 export const timelineService = {
   create: (data: CreateTimelineBody): Promise<CreateTimelineBody> => {
     return httpClient.post<CreateTimelineBody>("/timeline", data);
+  },
+
+  getAll: (): Promise<GetTimeLines> => {
+    return httpClient.get<GetTimeLines>("/timeline");
   },
 };
