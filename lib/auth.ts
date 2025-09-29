e0f0f04551300f0b4754f500d2b465243edd50a2
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2025-09-29 23:39:02 +0700

    config better auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,11 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { betterAuth } from "better-auth";
 
 const prisma = new PrismaClient();
+
 export const auth = betterAuth({
+	emailAndPassword: {
+		enabled: true,
+	},
   database: prismaAdapter(prisma, {
     provider: "postgresql", 
   }),
