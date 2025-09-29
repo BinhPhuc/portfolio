4eba22f35611f4aa89c90a70ae96cc80e4dab047
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2025-09-30 00:56:28 +0700

    client/server env

--- data/env/server.ts
+++ data/env/server.ts
@@ -3,7 +3,13 @@ import { z } from "zod";
 
 export const serverEnv = createEnv({
   server: {
-    BACKEND_API_URL: z.string().url(),
+    BACKEND_API_URL: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.string().min(1),
+    ADMIN_EMAIL: z.string().min(1),
+    ADMIN_PASSWORD: z.string().min(1),
+    ADMIN_NAME: z.string().min(1),
   },
   experimental__runtimeEnv: process.env,
   emptyStringAsUndefined: true,
