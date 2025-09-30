b2da5316ad70afc16a85caee7eb1000b58d36f7c
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2025-09-30 14:20:34 +0700

    init admin user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,28 @@
-import { serverEnv } from "@/data/env/server";
+import { config } from "dotenv";
+config();
 import { PrismaClient } from "../generated/prisma";
-import { v4 as uuidv4 } from "uuid";
+import { signUp } from "@/lib/auth-client";
+import { env } from "process";
 
 const prisma = new PrismaClient();
 
 async function main() {
   const existingAdmin = await prisma.user.findUnique({
     where: {
-      email: serverEnv.ADMIN_EMAIL,
+      email: process.env.ADMIN_EMAIL,
     },
   });
 
-  // TODO: Signup admin user via better-auth
+  if (!existingAdmin) {
+    await signUp.email({
+      email: process.env.ADMIN_EMAIL as string,
+      password: process.env.ADMIN_PASSWORD as string,
+      name: process.env.ADMIN_NAME as string,
+    });
+    console.log(`Admin user created with email: ${process.env.ADMIN_EMAIL}`);
+  } else {
+    console.log(`Admin user already exists: ${existingAdmin?.email}`);
+  }
 }
 
 main()
