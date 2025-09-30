cd8c0e39e8051c5b78be7ca1d7e180899d72c7e8
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2025-09-30 18:44:10 +0700

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,21 @@
-import type { NextRequest } from "next/server";
+import { auth } from "@/lib/auth";
+import { getCookieCache } from "better-auth/cookies";
+import { headers } from "next/headers";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  const session = await getCookieCache(request);
+  const pathName = request.nextUrl.pathname;
+
+  if (pathName.startsWith("/admin")) {
+    if (!session) {
+      return NextResponse.redirect(new URL("/auth/login", request.url));
+    }
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+  matcher: ["/admin/:path*"],
 };
